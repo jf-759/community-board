579a4d89213a4b521f357c2f1b3850b3982c5431
jf-759 <jane.fernandez@students.dominican.edu>
2025-09-20 13:45:29 -0700

    added data

---
+++ src/data/posts.js
@@ -0,0 +1,46 @@
+export const post = [
+    {
+        id: 1, 
+        group: "Stray Kids",
+        title: "KARMA",
+        description: "Stray Kids released their fourth full length album!",
+        image: "stray-kids.jpg",
+        date: "August 22, 2025"
+    },
+    {
+        id: 2,
+        group: "Monsta X",
+        title: "New Album 'THE X' Released",
+        description: "Monsta X drops their latest album in honor of their 10 years together with all 6 members together again!",
+        image: "monsta-x.jpg",
+        date: "September 1, 2025"
+
+    },
+    {
+        id: 3,
+        group: "aespa",
+        title: "New Album 'Rich Man' Released",
+        description: "Aespa drops their 6th mini-album, showing a bit of a different flare from their signature edge.",
+        image: "aespa.jpg",
+        date: "September 5, 2025"
+    },
+    {
+        id: 4,
+        group: "Seventeen",
+        title: "World Tour [NEW_]",
+        description: "Carats get ready! Seventeen's going on tour again!",
+        image: "seventeen.jpg",
+        date: "September 13, 2025 - December 21, 2025"
+    },
+    {
+        id: 5,
+        group: "STAYC",
+        title: "Stay Tuned",
+        description: "Stay tuned SWITH, STAYC is back on tour!",
+        image: "stayc.jpg",
+        date: "October 2, 2025 - October 23, 2025"
+    },
+    {
+        id: 6,
+    }, 
+]
